5dfa9bf482dd86f61da704b9e21d69cfaaa5028c
Rauny Martinelli <raunymartinelli@outlook.com>
2023-11-21 08:55:11 -0500

    lab Exercise week 11 about axios

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import PersonList from "./components/PersonList";
+
+function App() {
+  return (
+    <div>
+
+      <PersonList/>
+
+
+    </div>
+  );
+}
+
+export default App;
